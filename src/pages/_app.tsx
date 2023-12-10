89dc956a67ad0659bf3da555772fb6da841ee188
Abdulrahman Gamal <abdelrahmanabuglala@gmail.com>
2023-12-10 17:46:50 +0000

    Add sonner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
@@ -5,7 +6,12 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster richColors />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
